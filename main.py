ccf03025486294ccda33642d3cda313435e14c2b
Arffmies <arffmanvaltteri@gmail.com>
2026-09-25 09:37:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import random
 
-pelaajien_maara = int(input("Monta pelaajaa: "))
-print(pelaajien_maara)
+#pelaajien_maara = int(input("Monta pelaajaa: "))
+#print(pelaajien_maara)
+kirja = ["Ykköset", "Kakkoset", "Kolmoset",
+         "Neloset", "Viitoset", "Kuutoset"]
+nopat = [] 
+
+for i in range(5):
+    nopat.append(random.randint(1,6))
+print(nopat)
+
+for luku in range(1,7):
+    pisteet = nopat.count(luku) * luku
+    print(f"{kirja[luku -1]}: {pisteet}")
+
